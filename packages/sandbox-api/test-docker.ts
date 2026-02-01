2eca4771dc94c60322ac8f948d4757f99b8494f4
Igal Tabachnik <hmemcpy@gmail.com>
2026-02-01 03:28:58 +0200

    chore: remove test file

--- packages/sandbox-api/test-docker.ts
+++
@@ -1,9 +0,0 @@
-import Docker from "dockerode"
-
-const docker = new Docker({
-  socketPath: process.env["DOCKER_HOST"] ?? "/var/run/docker.sock",
-})
-
-docker.getImage("toolkata-env:bash").inspect()
-  .then(() => console.log("Image found!"))
-  .catch((err) => console.error("Error:", err.message))
